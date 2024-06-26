495486ce1d938554c05a1a00bfd0451c7498812f
liujun <liujun@guolichuxing.com>
2024-06-26 18:43:47 +0800

    消息通知

--- src/main/java/com/liuujun/idea/action/ActionDemo.java
+++ src/main/java/com/liuujun/idea/action/ActionDemo.java
@@ -1,5 +1,9 @@
 package com.liuujun.idea.action;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
@@ -8,5 +12,13 @@ public class ActionDemo extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+
+        NotificationGroup notificationGroup = NotificationGroupManager.getInstance()
+                .getNotificationGroup("PLUGIN-DEMO-ID");
+
+        Notification notification = notificationGroup
+                .createNotification("通知标题", "我是一个通知", NotificationType.INFORMATION);
+
+        notification.notify(e.getProject());
     }
 }
